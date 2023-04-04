105b9f6ad31df4c80cfe7693f03dd908f81e11ef
Romain <romain.gojard@gmail.com>
2023-04-04 09:20:35 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("YOOOO")
+jlefznlz
+
+const somme = (nombre1, nombre2) => {
+    return nombre1 + nombre2;
+}
+
+console.log(somme(3, 4))
+
+const plusGrandNombre = (tab) => {
+    tab.forEach(
+
+    )
+}
