7a611d3ef124e594376e17e547bf0e7a71ce1c87
Romain <romain.gojard@gmail.com>
2023-04-04 09:55:31 +0200

    Add function chaineSansVoyelles

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-console.log("YOOOO")
-
 const somme = (nombre1, nombre2) => {
     return nombre1 + nombre2;
 }
 
-console.log("somme de 3 et 4 : " + somme(3, 4))
+console.log("somme de 3 et 4 : " + somme(3, 4));
 
 const plusGrandNombre = (tab) => {
     if (tab.length === 0){
-        return false
+        return false;
     }else {
-        let resultat = tab[0]
+        let resultat = tab[0];
             tab.forEach(nombre =>{
                 if (nombre > resultat){
                     resultat = nombre;
                 }
         })
 
-        return resultat
+        return resultat;
     }
 }
-const tab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-console.log("plus grand nombre du tableau " + tab + " : " + plusGrandNombre(tab))
+const tab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("plus grand nombre du tableau " + tab + " : " + plusGrandNombre(tab));
+
+let chaineSansVoyelles = (chaine) => {
+    const voyelles = ['a', 'e', 'y', 'u', 'i', 'o'];
+    let nouvelleChaine = "";
+    [...chaine].forEach(lettre => {
+        if (!voyelles.includes(lettre)){
+            nouvelleChaine += lettre;
+        }
+    })
+    return nouvelleChaine;
+}
+
+const chaine = "Coucou les copains";
+console.log("Chaine avec voyelles : " + chaine + "Chaine sans voyelles : " + chaineSansVoyelles(chaine));
