673cc3bc3e9db531f7e9330ecf14c5ac11f84082
Romain <romain.gojard@gmail.com>
2023-04-04 09:29:55 +0200

    Add function plusGrandNombre

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 console.log("YOOOO")
-jlefznlz
 
 const somme = (nombre1, nombre2) => {
     return nombre1 + nombre2;
 }
 
-console.log(somme(3, 4))
+console.log("somme de 3 et 4 : " + somme(3, 4))
 
 const plusGrandNombre = (tab) => {
-    tab.forEach(
+    if (tab.length === 0){
+        return false
+    }else {
+        let resultat = tab[0]
+            tab.forEach(nombre =>{
+                if (nombre > resultat){
+                    resultat = nombre;
+                }
+        })
 
-    )
+        return resultat
+    }
 }
+const tab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log("plus grand nombre du tableau " + tab + " : " + plusGrandNombre(tab))
