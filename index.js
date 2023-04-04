d033cee7e3239b335f748f6b2ce3cb3d1192d6c8
Romain <romain.gojard@gmail.com>
2023-04-04 10:52:17 +0200

    Add function tableauPropriete

--- index.js
+++ index.js
@@ -39,3 +39,89 @@ const chainesTriees = (tabChaines) => tabChaines.sort();
 
 const tabChaines = ["Javascript", "Classique", "JePreferePython"];
 console.log("\nChaines pas triées : " + tabChaines + "\nChaines triées : " + chainesTriees(tabChaines));
+
+const nombreEnLettre = (nombre) => {
+    const unite = nombre % 10
+    const dizaine = (nombre - unite) / 10
+
+    let uniteEnLettre = '';
+    switch (unite) {
+        case 1 : uniteEnLettre = "un" ;
+        break
+        case 2 : uniteEnLettre = "deux";
+        break
+        case 3 : uniteEnLettre = "trois";
+        break
+        case 4 : uniteEnLettre = "quatre";
+        break
+        case 5 : uniteEnLettre = "cinq";
+        break
+        case 6 : uniteEnLettre = "six";
+        break
+        case 7 : uniteEnLettre = "sept";
+        break
+        case 8 : uniteEnLettre = "huit";
+        break
+        case 9 : uniteEnLettre = "neuf";
+        break
+    }
+
+    let dizaineEnLettre = '';
+    switch (dizaine) {
+        case 1 : dizaineEnLettre = "dix" ;
+            break
+        case 2 : dizaineEnLettre = "vingt";
+            break
+        case 3 : dizaineEnLettre = "trente";
+            break
+        case 4 : dizaineEnLettre = "quarante";
+            break
+        case 5 : dizaineEnLettre = "cinquante";
+            break
+        case 6 : dizaineEnLettre = "soixante";
+            break
+        case 7 : dizaineEnLettre = "soixante-dix";
+            break
+        case 8 : dizaineEnLettre = "quatre-vingt";
+            break
+        case 9 : dizaineEnLettre = "quatre-vingt-dix";
+            break
+    }
+
+    if(unite === 0){
+        return dizaineEnLettre;
+    }else{
+        return dizaineEnLettre + '-' + uniteEnLettre;
+    }
+}
+
+console.log("\nAfficher le nombre 42 en toute lettre : " + nombreEnLettre(42));
+
+const tableauPropriete = (tabObjets, propriete) => {
+    const tabResult = [];
+    tabObjets.forEach(objet => {
+        tabResult.push(objet[propriete]);
+    })
+    return tabResult;
+}
+
+const player = {
+    name: "",
+    active : true,
+    age : 0,
+    playertoString: function() {
+        return (`${this.name}. Player actif ? ${this.active}. Âge du joueur : ${this.age}`);
+    }
+};
+
+const player1 = Object.create(player);
+player1.name = "Chico";
+player1.age = 22;
+const player2 = Object.create(player);
+player2.name = "Lexio";
+player2.age = 22;
+const player3 = Object.create(player);
+player3.name = "XxSasukexX";
+player3.age = 12;
+console.log("\nAffichage de l'attribut âge pour les joueurs : " + player1.playertoString() + "\t" + player2.playertoString() + "\t" + player3.playertoString() + "\n" + tableauPropriete([player1, player2, player3], "name"));
+
